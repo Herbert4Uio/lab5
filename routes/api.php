6d90aaf2782b622466dd5650e57a5d58b3818687
Buster <bustabuster4@gmail.com>
2025-09-10 18:19:53 -0400

    Enpoint agregado Hola mundo

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/cristian', function (Request $request) {
+    return 'Hola mundo desde laravel';
+});
